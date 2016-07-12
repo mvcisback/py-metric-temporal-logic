8d5a32701c85d8827d62fcb04379d4b84127b606
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-07-11 23:31:52 -0700

    reimplemented milp using signal logic encoding

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from stl.ast import terms_lens, lineq_lens, walk, tree, and_or_lens
 from stl.ast import dt_sym, t_sym
-from stl.ast import LinEq, Interval, NaryOpSTL, Or, And, F, G, ModalOp, Neg
+from stl.ast import LinEq, Interval, NaryOpSTL, Or, And, F, G, ModalOp, Neg, Var
 from stl.parser import parse
