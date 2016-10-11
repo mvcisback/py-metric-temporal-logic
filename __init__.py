d658907c4c7d8bee7d0c5173b4db1204261d0824
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-10-11 10:59:57 -0700

    update import api

--- __init__.py
+++ __init__.py
@@ -2,3 +2,5 @@ from stl.utils import terms_lens, lineq_lens, walk, tree, and_or_lens
 from stl.ast import dt_sym, t_sym
 from stl.ast import LinEq, Interval, NaryOpSTL, Or, And, F, G, ModalOp, Neg, Var
 from stl.parser import parse
+from stl.synth import lex_param_project
+from stl.boolean_eval import pointwise_sat
