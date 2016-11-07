843304ed795e9e84473bf12333b40bc3f37df797
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-11-07 12:14:21 -0800

    fix test and update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='py-stl',
-      version='0.1',
+      version='0.11',
       description='TODO',
       url='http://github.com/mvcisback/py-stl',
       author='Marcell Vazquez-Chanlatte',
