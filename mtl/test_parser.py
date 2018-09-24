417d17781ef62b2e739dea49ee155ec5b46defb3
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-09-24 01:02:00 -0700

    remove residule traces dependencies

--- mtl/test_parser.py
+++ mtl/test_parser.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 from hypothesis import given
-from traces import TimeSeries
 
 import mtl
 from mtl.hypothesis import MetricTemporalLogicStrategy
-
-
-TS = {
-    "ap1": TimeSeries([(0, True), (0.1, True), (0.2, False)]),
-    "ap2": TimeSeries([(0, False), (0.2, True), (0.5, False)]),
-    "ap3": TimeSeries([(0, True), (0.1, True), (0.3, False)]),
-    "ap4": TimeSeries([(0, False), (0.1, False), (0.3, False)]),
-    "ap5": TimeSeries([(0, False), (0.1, False), (0.3, True)]),
-}
 
 
 @given(MetricTemporalLogicStrategy)
