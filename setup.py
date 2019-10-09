5f2071a02cdf2ddcc5db13c59552b42527f22102
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-10-08 19:18:14 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='metric-temporal-logic',
-    version='0.1.3',
+    version='0.1.4',
     description='A library for manipulating and evaluating metric temporal logic.',  # noqa
     url='http://github.com/mvcisback/py-metric-temporal-logic',
     author='Marcell Vazquez-Chanlatte',
